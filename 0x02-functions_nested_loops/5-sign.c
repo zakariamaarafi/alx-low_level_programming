7ebc3b3ce749df68961c05d54fae087b48161351
zakariamaarafi <maarafi.zakariaa@gmail.com>
2023-06-21 19:41:53 -0700

    5-sign.c mod 2

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,3 @@
-#include <limits.h>
 #include "main.h"
 
 /**
@@ -12,16 +11,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		return 1;
+		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return -1;
+		return (-1);
 	}
 	else
 	{
 		_putchar('0');
-		return 0;
+		return (0);
 	}
 }
