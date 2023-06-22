7bb4768d086ff998657175f0972082de6b0b8379
zakariamaarafi <maarafi.zakariaa@gmail.com>
2023-06-21 19:34:20 -0700

    5-sign.c error

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,29 +1,27 @@
+#include <limits.h>
 #include "main.h"
 
 /**
-  * print_sign - print + if n greater than zero,
-  *            0 if n is zero and - if n is less 
-  *            than zero.
-  * @n: takes integer type input for function.
-  *
-  * Return: 1 if +, 0 if 0 and -1 if -
+ * print_sign - checks the sign of a number
+ * @n: the number to check
+ *
+ * Return: 1 if n is positive, -1 if n is negative, 0 if n is zero
  */
-
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('43');
-		return (1);
+		_putchar('+');
+		return 1;
 	}
-	 else if (n == 0)
-        { 
-		_putchar(48);
-		return (0);
-	} 
+	else if (n < 0)
+	{
+		_putchar('-');
+		return -1;
+	}
 	else
 	{
-		_putchar('45');
-		return (-1);
+		_putchar('0');
+		return 0;
 	}
 }
