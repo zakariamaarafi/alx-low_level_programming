8c5c9f891a702bec46071ea4685793be788c6354
zakariamaarafi <maarafi.zakariaa@gmail.com>
2023-06-21 19:23:09 -0700

    5-sign.c modification 1

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,8 +3,8 @@
 /**
   * print_sign - print + if n greater than zero,
   *            0 if n is zero and - if n is less 
-  *            than zero
-  *@n: takes integer type input for function.
+  *            than zero.
+  * @n: takes integer type input for function.
   *
   * Return: 1 if +, 0 if 0 and -1 if -
  */
