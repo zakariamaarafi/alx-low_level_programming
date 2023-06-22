c69a9b85fc67066ef499a30ab4c1d9c1eb9b8a11
zakariamaarafi <maarafi.zakariaa@gmail.com>
2023-06-21 19:52:24 -0700

    9th task mod 3

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,34 +1,34 @@
 #include "main.h"
 
 /**
- * times_table - prints the times table from 0 to 9
- */
-void times_table(void)
-{
-	int i, j, result;
-
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			result = i * j;
-			if (j == 0)
-				printf("%2d", result);
-			else
-				printf("%3d", result);
-		}
-		printf("\n");
-	}
-}
-
-/**
  * main - entry point of the program
  *
  * Return: Always 0
  */
 int main(void)
 {
-	times_table();
-	return (0);
+    times_table();
+    return 0;
+}
+
+/**
+ * times_table - prints the times table from 0 to 9
+ */
+void times_table(void)
+{
+    int i, j, result;
+
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            result = i * j;
+            if (j == 0)
+                printf("%2d", result);
+            else
+                printf("%3d", result);
+        }
+        printf("\n");
+    }
 }
 
