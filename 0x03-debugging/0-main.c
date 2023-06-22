ecda52dccbd6468bdac05c35b08d3b2e437aecbd
zakariamaarafi <maarafi.zakariaa@gmail.com>
2023-06-22 08:33:20 -0700

    0-main.c mod5

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,32 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
-* main - Test function for positive or negative
-* Return: 0
-*/
+ * positive_or_negative - Check if a number is positive or negative
+ * @n: The number to check
+ *
+ * Return: void
+ */
+void positive_or_negative(int n)
+{
+    if (n > 0)
+        printf("%d is positive\n", n);
+    else if (n < 0)
+        printf("%d is negative\n", n);
+    else
+        printf("0 is zero\n");
+}
+
+/**
+ * main - Test function for positive or negative
+ *
+ * Return: 0
+ */
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
+    int i;
+
+    i = 0;
+    positive_or_negative(i);
+    return (0);
 }
