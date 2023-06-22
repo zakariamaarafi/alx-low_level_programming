265f6d62172ef96019217be66cb58aafab95cb68
zakariamaarafi <maarafi.zakariaa@gmail.com>
2023-06-21 18:00:46 -0700

    5-main.c code modification

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
- */
+ * * main - check the code.
+ *  *
+ *   * Return: Always 0.
+ *     */
 int main(void)
 {
     int r;
@@ -13,7 +13,7 @@ int main(void)
     _putchar(',');
     _putchar(' ');
     _putchar(r + '0');
-    _putchar('\n');
+i    _putchar('\n');
     r = print_sign(0);
     _putchar(',');
     _putchar(' ');
