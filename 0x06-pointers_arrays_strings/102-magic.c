184e782d79d586bc35e644ebc687009418dbc8e4
zakariamaarafi <maarafi.zakariaa@gmail.com>
2023-06-30 01:18:50 -0700

    102-magic.c code modification

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,24 +1,16 @@
-#include "main.h"
 #include <stdio.h>
 
 int main(void)
 {
-int n;
-int a[5];
-int *p;
+  int n;
+  int a[5];
+  int *p;
 
-a[2] = 1024;
-p = &n;
- /*
- * write your line of code here...
- * Remember:
- * - you are not allowed to use a
- * - you are not allowed to modify p
- * - only one statement
- * - you are not allowed to code anything else than this line of code
- */
-*(p + 5) = 98;
- /* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
-return (0);
+  a[2] = 1024;
+  p = &a[2];  // Point p to the address of a[2]
+
+  *(p) = 98;  // Assign 98 to the memory location pointed to by p
+
+  printf("a[2] = %d\n", a[2]);  // This will now print "a[2] = 98"
+  return 0;
 }
