40d74388cf7beb6e16efd75d3dab1530b8aa865a
zakaria maarafi <maarafi.zakariaa@gmail.com>
2023-08-01 04:50:04 -0700

    6-pop_listint.c,7-get_nodeint.c 2nd commit

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,16 +4,16 @@
 /**
  * pop_listint - it deletes the head node of the linked list
  *
- * @head: pointer to the first element in the linked list
+ * @head: pointer to the 1st  element in the linked list
  *
  * return: the data inside the elements that was deleted,
- * or 0 if the list is empty
+ * or 0 when  the list is empty
  */
 
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
-	int num;
+	int x;
 
 	if (!head || !*head)
 		return (0);
@@ -23,5 +23,5 @@ int pop_listint(listint_t **head)
 	free(*head);
 	*head = temp;
 
-	return (num);
+	return (x);
 }
