a5b782bad565b5e32bb94e8288d8d0752a1fd03c
zakaria maarafi <maarafi.zakariaa@gmail.com>
2023-08-01 02:41:27 -0700

    1-listint_len.c final commit

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,9 +3,8 @@
 
 /**
  * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
- *
- * Return: number of nodes
+ * @h: linked list type listint_t to be traversed
+ * Return: Number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
