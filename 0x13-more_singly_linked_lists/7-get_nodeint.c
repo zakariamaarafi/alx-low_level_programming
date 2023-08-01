4368835bb9d7e88551269a5ca6e856fb2626b49f
zakaria maarafi <maarafi.zakariaa@gmail.com>
2023-08-01 04:33:13 -0700

    6-pop_listint.c, 7-get_nodeint.c 1st commit

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - it returns the node to certain index in the
+ * linked list
+ * @head: first node in the linked list
+ * @index: index of the node to return
+ *
+ * Return: point to the node we're looking for or NULL
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int x = 0;
+	listint_t *temp = head;
+
+	while (temp && x < index)
+	{
+		temp = temp->next;
+		x++;
+	}
+
+	return (temp ? temp : NULL);
+}
