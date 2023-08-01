a37c5c57df8b30f3c631fc5785458edfc6e9c6a4
zakaria maarafi <maarafi.zakariaa@gmail.com>
2023-08-01 01:18:14 -0700

    lists.h,0-print_listint.c test commit 1

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,44 +1,33 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
  * struct listint_s - singly linked list
- * @x: integer value stored in the node
- * @next_node: points to the next node in the linked list
- *
- * Description: this is a structure to define a singly linked list node
- *              used for integers in a linked list it containing the
- *              value of x and a pointer to the next node *next_node.
+ * @x: an a integer
+ * @next_node: a pointer to the next node
  */
 typedef struct listint_s
 {
-    int x;
-    struct listint_s *next_node;
+int x;
+struct listint_s *next_node;
 } listint_t;
 
-/* Function prototypes*/
+/*functions prototypes*/
 
-int _putchar(char c);
-size_t print_listint(const listint_t *first_node);
-listint_t *reverse_listint(listint_t **first_node);
-size_t print_listint_safe(const listint_t *first_node);
-size_t free_listint_safe(listint_t **first_node);
-listint_t *find_listint_loop(listint_t *first_node);
-listint_t *add_nodeint_end(listint_t **first_node, const int x);
-void free_listint(listint_t *first_node);
-void free_listint2(listint_t **first_node);
-int pop_listint(listint_t **first_node);
-size_t listint_len(const listint_t *first_node);
-listint_t *add_nodeint(listint_t **first_node, const int x);
-listint_t *get_nodeint_at_index(listint_t *first_node, unsigned int index);
-int sum_listint(listint_t *first_node);
-listint_t *insert_nodeint_at_index(listint_t **first_node, unsigned int idx, int x);
-int delete_nodeint_at_index(listint_t **first_node, unsigned int index);
-
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
-
-
