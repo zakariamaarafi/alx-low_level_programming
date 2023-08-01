15894e34d3f6c3dd60134abd18dd1aa6e925c140
zakaria maarafi <maarafi.zakariaa@gmail.com>
2023-08-01 02:30:00 -0700

    commit demo

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,20 +1,23 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
- * @x: an a integer
- * @next_node: a pointer to the next node
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for alx project
  */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-/*functions prototypes*/
-
-int putchar(int c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
