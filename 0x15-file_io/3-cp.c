daffbe3dd2203368147f7b073bcd4cd8e8381c29
zakaria maarafi <maarafi.zakariaa@gmail.com>
2023-08-08 13:52:25 -0700

    main.h 3.cp.c headers mod

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * create_buff - to zllocate 1024 bytes for buffer.
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,8 +5,6 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/types.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
