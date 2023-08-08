02127572f5d6d4135375962948f1154ef6fd96e1
zakaria maarafi <maarafi.zakariaa@gmail.com>
2023-08-07 18:50:54 -0700

    1-create_file.c more rectifs

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		while (text_content[len])
-			len+;
+			len++;
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
@@ -29,10 +29,10 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		w = write(fd, text_content, len);
-		if (w ==-1)
+		if (w == -1)
 		{
 			close(fd);
-			return (1);
+			return (-1);
 		}
 	}
 
