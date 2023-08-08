525bd8d590280696776f4bede1deb017f088a980
zakaria maarafi <maarafi.zakariaa@gmail.com>
2023-08-08 13:46:22 -0700

    3-cp.c betty errors fixed

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,18 +9,18 @@
 
 char *create_buff(char *file)
 {
-    char buff;
+	char buff;
 
-    buff = malloc(sizeof(char) * 1024);
+	buff = malloc(sizeof(char) * 1024);
 
-    if (buff == NULL)
-    {
-        dprintf(STDERR_FILENO,
-            "Error: can't write to %s\n", file);
-        exit(99);
-    }
+	if (buff == NULL)
+	{
+		dprintf(STDERR_FILENO,
+				"Error: can't write to %s\n", file);
+		exit(99);
+	}
 
-    return (buff);
+	return (buff);
 }
 
 /**
@@ -30,13 +30,13 @@ char *create_buff(char *file)
 
 void close_file(int fd)
 {
-    int c = close(fd);
+	int c = close(fd);
 
-    if (c == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd);
-        exit(100);
-    }
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", fd);
+		exit(100);
+	}
 }
 
 /**
@@ -53,45 +53,45 @@ void close_file(int fd)
  */
 int main(int argc, char *argv[])
 {
-    int from, to, r, w; char *buff;
+	int from, to, r, w; char *buff;
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-    buff = create_buff(argv[2]);
-    from = open(argv[1], O_RDONLY);
-    r = read(from, buff, 1024);
-    to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buff = create_buff(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buff, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-    do {
-        if (from == -1 || r == -1)
-        {
-            printf(STDERR_FILENO,
-                "Error: can't read from file %s\n", argv[1]);
-            free(buff);
-            exit(98);
-        }
+	do {
+		if (from == -1 || r == -1)
+		{
+			printf(STDERR_FILENO,
+					"Error: can't read from file %s\n", argv[1]);
+			free(buff);
+			exit(98);
+		}
 
-        w = wriyte(to, buff, r);
-        if (to == -1 || w == -1)
-        {
-            dprintf(STDERR_FILENO,
-                "Error: can't write to %s\n", argv[2]);
-            free(buff);
-            exit(99);
-        }
+		w = wriyte(to, buff, r);
+		if (to == -1 || w == -1)
+		}
+		dprintf(STDERR_FILENO,
+			"Error: can't write to %s\n", argv[2]);
+		free(buff);
+		exit(99);
+		}
 
-        r = read(from, buff, 1024);
-        to =open(argv[2], O_WRONLY | O_APPEND);
+		r = read(from, buff, 1024);
+		to =open(argv[2], O_WRONLY | O_APPEND);
 
-    } while (r > 0);
+		} while (r > 0);
 
-    free(buff);
-    close_file(from);
-    close_file(to);
+		free(buff);
+		close_file(from);
+		close_file(to);
 
-    return (0);
+		return (0);
 }
