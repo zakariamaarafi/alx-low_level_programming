9666024e1ec02f2aebf6ed88f158f167d6c38e54
zakaria maarafi <maarafi.zakariaa@gmail.com>
2023-08-08 16:36:19 -0700

    3-cp.c main.h commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-char *create_buffer(char *file);
-void close_file(int fd);
 
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
