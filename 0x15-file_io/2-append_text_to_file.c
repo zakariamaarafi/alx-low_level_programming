3a99961025f28248eafb440a3a16889e12891b51
zakaria maarafi <maarafi.zakariaa@gmail.com>
2023-08-07 19:24:51 -0700

    2-append_text_to_file.c betty trailing whitespace

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,25 +13,25 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, w;
-    int len = 0;
+	int fd, w;
+	int len = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+	return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[len])
-            len++;
-    }
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
 
-    fd = open(filename, O_WRONLY | O_APPEND);
-    w = write(fd, text_content, len);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	w = write(fd, text_content, len);
 
-    if (fd == -1 || w == -1)
-        return (-1);
+	if (fd == -1 || w == -1)
+		return (-1);
 
-    close(fd);
+	close(fd);
 
-    return (1);
+	return (1);
 }
