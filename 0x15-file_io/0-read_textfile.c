1976fdb8e31b0c944de28884e9745dce64191539
zakaria maarafi <maarafi.zakariaa@gmail.com>
2023-08-07 18:18:36 -0700

    0-read_textfile.c code error ()

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return 0;
+		return (0);
 	buf = malloc(sizeof(char) * letters);
 	r = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, r);
