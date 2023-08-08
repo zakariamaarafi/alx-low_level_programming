f07a3b58e95cfbdf4616ba4037e680ed1993e3e5
zakaria maarafi <maarafi.zakariaa@gmail.com>
2023-08-08 16:18:52 -0700

    3-cp.c more modifications

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *create_buff(char *file);
+char *create_buffer(char *file);
 void close_file(int fd);
 
 /**
@@ -12,7 +12,7 @@ void close_file(int fd);
  * Return: pointer to the new allocated buffer.
  */
 
-char *create_buff(char *file)
+char *create_buffer(char *file)
 {
 	char *buff;
 
@@ -20,7 +20,7 @@ char *create_buff(char *file)
 
 	if (buff == NULL)
 	{
-		dprintf(STDERR_FILENO,"Error: can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error: can't write to %s\n", file);
 		exit(99);
 	}
 
@@ -57,7 +57,8 @@ void close_file(int fd)
  */
 int main(int argc, char *argv[])
 {
-	int from, to, r, w; char *buff;
+	int from, to, r, w;
+	char *buff;
 
 	if (argc != 3)
 	{
@@ -65,7 +66,7 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	buff = create_buff(argv[2]);
+	buff = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buff, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
@@ -73,7 +74,7 @@ int main(int argc, char *argv[])
 	do {
 		if (from == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO,"Error: can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: can't read from file %s\n", argv[1]);
 			free(buff);
 			exit(98);
 		}
@@ -81,7 +82,7 @@ int main(int argc, char *argv[])
 		w = write(to, buff, r);
 		if (to == -1 || w == -1)
 		}
-		dprintf(STDERR_FILENO,"Error: can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
 		free(buff);
 		exit(99);
 		}
